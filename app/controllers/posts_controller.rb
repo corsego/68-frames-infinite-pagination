a9b377b8f6c8e713f8ec8dedcb26280eb69bc85f
Yaro <yshmarov@gmail.com>
2022-03-20 16:06:49 +0000

    seeds and pagination for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    # @posts = Post.all
+    @pagy, @posts = pagy(Post.order(created_at: :desc), items: 5)
   end
 
   # GET /posts/1 or /posts/1.json
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 100.times do 
   Comment.create(body: Faker::Quote.famous_last_words)
+  Post.create(title: Faker::Music.band, image_url: Faker::Avatar.image)
 end
